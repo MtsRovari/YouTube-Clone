7fe2800a2c09ec8a351bfd386e2f80e4449bfc24
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-04-16 13:55:56 -0300

    setup project

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Mateus Rovari on 16/04/19.
+//  Copyright © 2019 Mateus Rovari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
